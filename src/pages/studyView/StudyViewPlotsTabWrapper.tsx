82945853b537e5c6248d89a1b3bf5c6a9839f8b5
alisman <lisman.aaron@gmail.com>
2024-09-24 15:33:41 -0400

    Add loading behavior for study view plots tab

--- src/pages/studyView/StudyViewPlotsTabWrapper.tsx
+++ src/pages/studyView/StudyViewPlotsTabWrapper.tsx
@@ -3,69 +3,76 @@ import { StudyViewPageStore } from 'pages/studyView/StudyViewPageStore';
 import StudyViewURLWrapper from 'pages/studyView/StudyViewURLWrapper';
 import { observer } from 'mobx-react';
 import PlotsTab from 'shared/components/plots/PlotsTab';
+import LoadingIndicator from 'shared/components/loadingIndicator/LoadingIndicator';
 
 export const PlotsTabWrapper: React.FunctionComponent<{
     store: StudyViewPageStore;
     urlWrapper: StudyViewURLWrapper;
 }> = observer(function({ store, urlWrapper }) {
-    return (
-        <PlotsTab
-            filteredSamplesByDetailedCancerType={
-                store.filteredSamplesByDetailedCancerType
-            }
-            mutations={store.mutations}
-            studies={store.queriedPhysicalStudies}
-            molecularProfileIdSuffixToMolecularProfiles={
-                store.molecularProfileIdSuffixToMolecularProfiles
-            }
-            entrezGeneIdToGene={store.entrezGeneIdToGeneAll}
-            sampleKeyToSample={store.sampleSetByKey}
-            genes={store.allGenes}
-            clinicalAttributes={store.clinicalAttributes}
-            genesets={store.genesets}
-            genericAssayEntitiesGroupByMolecularProfileId={
-                store.genericAssayEntitiesGroupedByProfileId
-            }
-            studyIds={store.queriedPhysicalStudyIds}
-            molecularProfilesWithData={store.molecularProfilesInStudies}
-            molecularProfilesInStudies={store.molecularProfilesInStudies}
-            annotatedCnaCache={store.annotatedCnaCache}
-            annotatedMutationCache={store.annotatedMutationCache}
-            structuralVariantCache={store.structuralVariantCache}
-            studyToMutationMolecularProfile={
-                store.studyToMutationMolecularProfile
-            }
-            studyToMolecularProfileDiscreteCna={
-                store.studyToMolecularProfileDiscreteCna
-            }
-            clinicalDataCache={store.clinicalDataCache}
-            patientKeyToFilteredSamples={store.patientKeyToFilteredSamples}
-            numericGeneMolecularDataCache={store.numericGeneMolecularDataCache}
-            coverageInformation={store.coverageInformation}
-            filteredSamples={store.selectedSamples}
-            genesetMolecularDataCache={store.genesetMolecularDataCache}
-            genericAssayMolecularDataCache={
-                store.genericAssayMolecularDataCache
-            }
-            studyToStructuralVariantMolecularProfile={
-                store.studyToStructuralVariantMolecularProfile
-            }
-            driverAnnotationSettings={store.driverAnnotationSettings}
-            studyIdToStudy={store.studyIdToStudy.result}
-            structuralVariants={store.structuralVariants.result}
-            hugoGeneSymbols={store.allHugoGeneSymbols.result}
-            selectedGenericAssayEntitiesGroupByMolecularProfileId={
-                store.selectedGenericAssayEntitiesGroupByMolecularProfileId
-            }
-            molecularProfileIdToMolecularProfile={
-                store.molecularProfileIdToMolecularProfile
-            }
-            urlWrapper={urlWrapper}
-            hasNoQueriedGenes={true}
-            genePanelDataForAllProfiles={
-                store.genePanelDataForAllProfiles.result
-            }
-            patients={store.patients}
-        />
-    );
+    if (store.allHugoGeneSymbols.isComplete) {
+        return (
+            <PlotsTab
+                filteredSamplesByDetailedCancerType={
+                    store.filteredSamplesByDetailedCancerType
+                }
+                mutations={store.mutations}
+                studies={store.queriedPhysicalStudies}
+                molecularProfileIdSuffixToMolecularProfiles={
+                    store.molecularProfileIdSuffixToMolecularProfiles
+                }
+                entrezGeneIdToGene={store.entrezGeneIdToGeneAll}
+                sampleKeyToSample={store.sampleSetByKey}
+                genes={store.allGenes}
+                clinicalAttributes={store.clinicalAttributes}
+                genesets={store.genesets}
+                genericAssayEntitiesGroupByMolecularProfileId={
+                    store.genericAssayEntitiesGroupedByProfileId
+                }
+                studyIds={store.queriedPhysicalStudyIds}
+                molecularProfilesWithData={store.molecularProfilesInStudies}
+                molecularProfilesInStudies={store.molecularProfilesInStudies}
+                annotatedCnaCache={store.annotatedCnaCache}
+                annotatedMutationCache={store.annotatedMutationCache}
+                structuralVariantCache={store.structuralVariantCache}
+                studyToMutationMolecularProfile={
+                    store.studyToMutationMolecularProfile
+                }
+                studyToMolecularProfileDiscreteCna={
+                    store.studyToMolecularProfileDiscreteCna
+                }
+                clinicalDataCache={store.clinicalDataCache}
+                patientKeyToFilteredSamples={store.patientKeyToFilteredSamples}
+                numericGeneMolecularDataCache={
+                    store.numericGeneMolecularDataCache
+                }
+                coverageInformation={store.coverageInformation}
+                filteredSamples={store.selectedSamples}
+                genesetMolecularDataCache={store.genesetMolecularDataCache}
+                genericAssayMolecularDataCache={
+                    store.genericAssayMolecularDataCache
+                }
+                studyToStructuralVariantMolecularProfile={
+                    store.studyToStructuralVariantMolecularProfile
+                }
+                driverAnnotationSettings={store.driverAnnotationSettings}
+                studyIdToStudy={store.studyIdToStudy.result}
+                structuralVariants={store.structuralVariants.result}
+                hugoGeneSymbols={store.allHugoGeneSymbols.result}
+                selectedGenericAssayEntitiesGroupByMolecularProfileId={
+                    store.selectedGenericAssayEntitiesGroupByMolecularProfileId
+                }
+                molecularProfileIdToMolecularProfile={
+                    store.molecularProfileIdToMolecularProfile
+                }
+                urlWrapper={urlWrapper}
+                hasNoQueriedGenes={true}
+                genePanelDataForAllProfiles={
+                    store.genePanelDataForAllProfiles.result
+                }
+                patients={store.patients}
+            />
+        );
+    } else {
+        return <LoadingIndicator isLoading={true} />;
+    }
 });
